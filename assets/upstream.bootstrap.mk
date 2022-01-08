7da4d6cf1188cd960ad1e529ba125da9d763c9c2
Bert van der Weerd <bert@stanzabird.nl>
2022-01-08 23:19:16 +0100

    bugfix

--- assets/upstream.bootstrap.mk
+++ assets/upstream.bootstrap.mk
@@ -1,6 +1,6 @@
 ## handle different upstreams, like developer,nightly, or distro specific upstream cases
 upstream_filename=bootstrap.py
-upstream_dirname=mozilla-nightly
+upstream_dirname=mozilla-unified
 $(upstream_filename) :
 	wget -q https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py
 ##
@@ -10,5 +10,5 @@ clean_upstream_dir :
 	rm -rf $(upstream_dirname)
 create_lw_from_upstream_dir :
 	python3 bootstrap.py --no-interactive --application-choice=browser
-	mv  $(upstream_dirname) librewolf-$(version)
+	mv $(upstream_dirname) librewolf-$(version)
 
