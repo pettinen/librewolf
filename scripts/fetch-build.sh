9423e9db41280339ea89b40bf93ad52f013a0e05
Bert van der Weerd <bert@stanzabird.nl>
2022-01-08 20:28:17 +0100

    refactor to source_build

--- scripts/fetch-build.sh
+++ scripts/fetch-build.sh
@@ -3,15 +3,15 @@
 set -e
 
 fetch_and_extract() {
-    rm -rf version release
+    rm -rf version source_release
     wget -q -O version "https://gitlab.com/librewolf-community/browser/source/-/raw/main/version"
-    wget -q -O release "https://gitlab.com/librewolf-community/browser/source/-/raw/main/release"
+    wget -q -O source_release "https://gitlab.com/librewolf-community/browser/source/-/raw/main/release"
     
-    rm -f "librewolf-$(cat version)-$(cat release).source.tar.gz"
-    wget -O "librewolf-$(cat version)-$(cat release).source.tar.gz" "https://gitlab.com/librewolf-community/browser/source/-/jobs/artifacts/main/raw/librewolf-$(cat version)-$(cat release).source.tar.gz?job=build-job"
+    rm -f "librewolf-$(cat version)-$(cat source_release).source.tar.gz"
+    wget -O "librewolf-$(cat version)-$(cat source_release).source.tar.gz" "https://gitlab.com/librewolf-community/browser/source/-/jobs/artifacts/main/raw/librewolf-$(cat version)-$(cat source_release).source.tar.gz?job=build-job"
     
     rm -rf librewolf-$(cat version)
-    tar xf librewolf-$(cat version)-$(cat release).source.tar.gz
+    tar xf librewolf-$(cat version)-$(cat source_release).source.tar.gz
 
     # here would be a great spot to insert system dependent stuff like mozconfig/patches.
 }
