4331a8590f57db90d06f11219f72a10952458ee2
Bert van der Weerd <bert@stanzabird.nl>
2022-04-14 11:20:46 +0200

    added git-patchtree.sh

---
+++ scripts/git-patchtree.sh
@@ -0,0 +1,38 @@
+#
+# taken from ./scripts/check-patchfail.sh
+#
+# script: it 1) extract source 2) git init 2) grab filenames from diff and git add them
+#
+
+
+if [ ! -f version ]; then
+    echo "error: 'version' does not exist. Are you in the right folder?"
+    exit 1
+fi
+firefox=firefox-$(cat version).source.tar.xz
+if [ ! -f "$firefox" ]; then
+    echo "error: '$firefox' does not exist."
+    exit 1
+fi
+
+
+
+if [ ! -f "$1" ]; then
+    echo "git-patchtree.sh: error, first argument must be a patch file"
+    exit 1
+fi
+
+
+
+echo "Extracting '$firefox'..."
+rm -rf firefox-$(cat version)
+tar xf $firefox
+echo ""
+
+cd firefox-$(cat version) && \
+    git init && \
+    git add $(grep '+++' "../$1" | awk '{print $2}' | sed s/^b/./) && \
+    git commit -am "original" && \
+    patch -p1 -i "../$1" && \
+    git commit -am "patch"
+cd ..
